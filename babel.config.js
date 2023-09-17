043bdb1c173aa28807a0fcfc24896cf2f00e7124
Marcelo Belkiman <marcelobelkiman@gmail.com>
2023-09-17 15:38:50 -0300

    use absolute path

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '^@/(.+)': './src/\\1',
+        },
+        cwd: 'packagejson',
+      },
+    ],
+  ],
 };
